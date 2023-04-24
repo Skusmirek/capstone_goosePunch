14c0240dd991b4c0069b39261497dc77ccf65309
Isaiah Winegarden <groovygabriel26@gmail.com>
2023-04-24 12:38:57 -0500

    Added Fight Introduction screen and arena

--- fightOne.js
+++ fightOne.js
@@ -1,7 +1,13 @@
 $(function() {
   
-  // Get a reference to and set the Introduction Image to fit the whole screen
-  $introImage = $('#introImage');
+  // Get reference to introduction image and image used for the arena
+  $introImage = $('#introImageOne');
+  $arena = $('#arenaImage');
+
+
+  $arena.fadeOut(.01)
+
+  $introImage.fadeOut(.01);
 
   $introImage.css({
     width: '100%',
@@ -11,5 +17,18 @@ $(function() {
     top: 0
   })
 
+  $arena.css({
+    width: '100%',
+    height: '100%',
+    position: 'fixed',
+    left: 0,
+    top: 0
+  })
 
+  $introImage.fadeIn(1500);
+
+
+  $introImage.delay(2500).fadeOut(1500);
+
+  $arena.delay(5000).fadeIn(500);
 });
