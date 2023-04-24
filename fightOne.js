cd8c9894fabbc150654df1d88049bfec8e809369
Isaiah Winegarden <groovygabriel26@gmail.com>
2023-04-24 11:19:33 -0500

    Added Images folder

---
+++ fightOne.js
@@ -0,0 +1,17 @@
+$(function() {
+  
+  // Get a reference to and set the Introduction Image to fit the whole screen
+  $introImage = $('#introImage');
+
+  $introImage.css({
+    width: '100%',
+    height: '100%',
+    position: 'fixed',
+    left: 0,
+    top: 0
+  })
+
+
+
+
+});
