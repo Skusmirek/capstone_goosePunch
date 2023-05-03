7ee89ffd7a16f5a9ef203ebeabc7854a087585dc
CrispityBits <peytonkringle@gmail.com>
2023-05-03 12:03:01 -0500

    [finishes #184971443] Changed file sctructure

--- base.js
+++ base.js
@@ -1,5 +1,9 @@
-if(window.jQuery) {  
-  console.log('jQuery is installed!');
-} else {
-  console.log('jQuery is not installed!');
+$(function() {
+
+  console.log("window ready");
+
+});
+
+function startButtonClick() {
+  
 }
