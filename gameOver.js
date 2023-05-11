58f12212a0d731eb4d1445e0096182785952f08c
Isaiah Winegarden <groovygabriel26@gmail.com>
2023-05-11 16:00:13 -0500

    [finishes #185161603] Added Game Over screen

---
+++ gameOver.js
@@ -0,0 +1,16 @@
+$(function() {
+
+  $gameOverScreen = $("#gameOver");
+
+  $gameOverScreen.css({
+    width: '100%',
+    height: '100%',
+    position: 'fixed',
+    left: 0,
+    top: 0
+  })
+  $gameOverScreen.fadeIn(50);
+  
+
+
+});
